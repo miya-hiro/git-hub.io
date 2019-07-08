a58cd530b9f74555a5274fb83f312d77f285ce30
miya-hiro <52573499+miya-hiro@users.noreply.github.com>
2019-07-08 15:58:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -249,7 +249,7 @@ if(!empty($_POST)){
     <meta charset="utf-8">
     <title>ゲーム！きのこちゃん</title>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=yes">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=swap" rel="stylesheet">   
     </head>
